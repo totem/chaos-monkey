29e6fadecfdfb29ef092eb55ebd6e3350c13a00b
Sukrit Khera <sukrit007@gmail.com>
2016-03-30 10:53:53 -0700

    Fix chaos

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ RUN apk add --no-cache --update \
       python \
       python-dev \
       py-pip \
-      openssl \
       bash \
+      openssl \
   && wget -O /tmp/awscli-bundle.zip "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" \
   && unzip -o -d /tmp /tmp/awscli-bundle.zip \
   && /tmp/awscli-bundle/install -b /usr/local/bin/aws \
--- chaos.sh
+++ chaos.sh
@@ -6,7 +6,9 @@ if [ ! -z $DRY_RUN ] ;then
 fi
 INSTANCES="$(aws ec2 describe-instances --region=$REGION --filters 'Name=tag:Name,Values='$TAG_NAME | grep 'InstanceId')"
 if [ "$INSTANCES" != "" ]; then
-  echo "$INSTANCES" | xargs echo -n | awk 'BEGIN { RS = ","; FS = ": ";ORS = " "}{print $2}' | xargs shuf -n 1 -e -z | xargs -0 -I {}  aws ec2 reboot-instances $DRY_RUN_FLAG --region=$REGION --instance-ids="{}"
+  REBOOT_INSTANCE=$(echo "IGONRE_BUG: IGNORE_BUG,$INSTANCES" | xargs echo -n | awk 'BEGIN { RS = ","; FS = ": ";ORS = " "}{print $2}' | xargs shuf -n 1 -e -z)
+  echo "chaos-monkey: Rebooting instance $REBOOT_INSTANCE "
+  aws ec2 reboot-instances $DRY_RUN_FLAG --region=$REGION --instance-ids="$REBOOT_INSTANCE"
 else
   echo "No instances found matching the criteria..."
   exit 1
