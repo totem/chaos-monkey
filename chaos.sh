f9123dd18481c8b5ce76594b7d1a454e2752d8fa
Sukrit Khera <sukrit007@gmail.com>
2016-03-30 11:52:40 -0700

    Add ability to enable/disable on odd/even weeks

--- chaos.sh
+++ chaos.sh
@@ -1,9 +1,23 @@
 #!/bin/bash -e
 REGION="${REGION:-us-west-1}"
 TAG_NAME="${TAG_NAME:-totem-totem-develop}"
+RUN_ON_EVEN_WEEKS="${RUN_ON_EVEN_WEEKS:-true}"
+RUN_ON_ODD_WEEKS="${RUN_ON_ODD_WEEKS:-true}"
 if [ ! -z $DRY_RUN ] ;then
   DRY_RUN_FLAG="--dry-run"
 fi
+week=$(date +%U)
+
+if [ "$RUN_ON_EVEN_WEEKS" == "false" ] && [ $(($week % 2)) == 0 ]; then
+  echo "chaos-monkey: Not active on even weeks. Set RUN_ON_EVEN_WEEKS to true for creating chaos on even weeks"
+  exit 0
+fi
+
+if [ "$RUN_ON_ODD_WEEKS" == "false" ] && [ $(($week % 2)) == 1 ]; then
+  echo "chaos-monkey: Not active on odd weeks. Set RUN_ON_ODD_WEEKS to true for creating chaos on odd weeks"
+  exit 0
+fi
+
 INSTANCES="$(aws ec2 describe-instances --region=$REGION --filters 'Name=tag:Name,Values='$TAG_NAME | grep 'InstanceId')"
 if [ "$INSTANCES" != "" ]; then
   REBOOT_INSTANCE=$(echo "IGONRE_BUG: IGNORE_BUG,$INSTANCES" | xargs echo -n | awk 'BEGIN { RS = ","; FS = ": ";ORS = " "}{print $2}' | xargs shuf -n 1 -e -z)
